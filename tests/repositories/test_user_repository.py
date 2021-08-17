eb012aa2c16dfdaa140a8598cf26be1431d62c20
Osmay Cruz <osmay.cruz@gmail.com>
2021-08-16 23:01:08 -0400

    Added unittets

--- tests/repositories/test_user_repository.py
+++ tests/repositories/test_user_repository.py
@@ -1,5 +1,6 @@
 import pytest
 from tests import app_client
+from sqlalchemy.exc import IntegrityError
 from web_app.repositories.user_repo import UserRepo
 
 
@@ -13,6 +14,19 @@ class TestUserRepository():
                 'email': 'a@a.com',
                 'gender': 'M'
             }
-
             new_user = repo.add(user_data)
             assert new_user.id == 1
+
+    def test_add_user_missing_fields(self, app_client):
+        with app_client.application.app_context():
+            repo = UserRepo()
+            user_data = {
+                'username': 'test_user',
+            }
+            try:
+                new_user = repo.add(user_data)
+                assert new_user.id == 1
+            except IntegrityError as err:
+                assert True
+            else:
+                assert False, "The integrity error wasn't throw"
